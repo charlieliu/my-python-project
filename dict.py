b5462eafede08608628197217b47460e19efd420
Charlie LIU <liuchangli0107@gmail.com>
2023-01-16 17:26:41 +0800

    modify dict.py for teaching

--- dict.py
+++ dict.py
@@ -2,50 +2,79 @@
 Mapping Types — dict
 https://docs.python.org/3/library/stdtypes.html#dict
 '''
-
-def printAll():
+def printA(title=''):
+    print('==========', title, '==========')
+    print('a', type(a), a)
+def printAll(title=''):
+    print('==========', title, '==========')
     print('a', type(a), a)
     print('b', type(b), b)
     print('c', type(c), c)
     print('d', type(d), d)
     print('e', type(e), e)
     print('f', type(f), f)
-    print('deep',   type(deep),     deep)
-    print('shallow',type(shallow),  shallow)
-    print('====================')
-def printCopy():
+def printCopy(title=''):
+    print('==========', title, '==========')
     print('a',      type(a),        a)
     print('deep',   type(deep),     deep)
     print('shallow',type(shallow),  shallow)
-    print('====================')
-
+# 建立 Dictionary 的方法
 a = dict(one=1, two=2, three=3)
 b = {'one': 1, 'two': 2, 'three': 3}
 c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
 d = dict([('two', 2), ('one', 1), ('three', 3)])
 e = dict({'three': 3, 'one': 1, 'two': 2})
 f = dict({'one': 1, 'three': 3}, two=2)
+printAll('建立 Dictionary 的方法')
+
+# 新增 Dictionary 元素的方法
+a['four'] = 4
+a = dict(a, five=5)
+printA('新增 Dictionary 元素的方法')
+
+# 讀取 Dictionary 元素的方法
+print('==========', '讀取 Dictionary 元素的方法', '==========')
+print('a[\'one\']:', a['one'])
+for key in a: print('key:', type(key), key, ', value:', type(a[key]), a[key])
+print('\'one\' in a:', 'one' in a)
+print('\'one\' not in a:', 'one' not in a)
+print('a.get(\'one\'):', a.get('one', 0))
+print('a.get(\'six\'):', a.get('six', 0))
+
+# 修改 Dictionary 元素的方法
+a['four'] = 44
+a = dict(a, five=55)
+printA('修改 Dictionary 元素的方法')
+
+# 複製 Dictionary 元素的方法
+a['zero'] = a['four']
+a['six'] = int(a['zero'])
+printA('複製 Dictionary 元素的方法')
+
+# 複製 Dictionary 的方法
 deep = a.copy() # deep copy
 shallow = a     # shallow copy
-printAll()
-# print('list', list(d))
-# print('len', len(d))
-# print('key', d['one'])
-# for key in d: print('key', type(key), key)
-# print('in', 'one' in d)
-# print('not in', 'one' not in d)
-# print('iter(d)', iter(d))
-# print('clear()', d.clear())
-# print('d', type(d), d)
-# print('====================')
-# del a['one']
-# printCopy()
-# print('get()', a.get('one', 0))
-# print('items()', a.items())
-# for item in a.items(): print('item', type(item), item)
-# print('keys()', a.keys())
-# for key in a.keys(): print('key', type(key), key)
-# a['one'] = 100
-# print('pop()', a.pop('one'))
-# print('pop()', a.pop('one', 0))
-# printCopy()
+printCopy('複製 Dictionary 的方法')
+
+# 刪除 Dictionary 元素的方法
+del a['five']
+x = a.pop('four')
+printCopy('刪除 Dictionary 元素的方法')
+print('x', type(x), x)
+
+# 刪除 Dictionary 所有元素的方法
+a.clear()
+printCopy('刪除 Dictionary 所有元素的方法')
+
+# 其他方法
+print('==========', '其他方法', '==========')
+print('list(b)', list(b))
+print('len(b)', len(b))
+print('iter(b)', iter(b))
+print('b.items()', b.items())
+for item in b.items(): print('item', type(item), item)
+print('keys()', b.keys())
+for key in b.keys(): print('key', type(key), key)
+print('b.items()', b.values())
+for item in b.values(): print('item', type(item), item)
+
